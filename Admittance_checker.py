4c8b00b0e5af5d46c627d12292049785173f4d65
Vincent Allam <80867514+VincentA004@users.noreply.github.com>
2022-10-25 10:49:18 -0500

    Update Admittance_checker.py

--- Admittance_checker.py
+++ Admittance_checker.py
@@ -1,4 +1,7 @@
+import datetime as dt
 import time
+import pyautogui as py
+import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -13,23 +16,24 @@ def main():
     
 def Austin():
     url = "https://utdirect.utexas.edu/apps/adm/mystatus/statuscheck/admission/00/"
-
-    username = input("enter username: ")
-    password = input('enter password: ')
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.maximize_window()
-    driver.get(url)
-    time.sleep(1)
-    u = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="username"]')))
-    u.send_keys(username)
-    
-    p = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="password"]')))
-    p.send_keys(password)
-    
-    xpath = '//*[@id="login-button"]/input'
-    a = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH,xpath)))
-    a.click()
-    time.sleep(10)
+    if __name__ == "__main__":
+        username = input("enter username: ")
+        password = input('enter password: ')
+        options = webdriver.ChromeOptions()
+        driver = uc.Chrome(version_main = 103 ,options = options)
+        driver.maximize_window()
+        driver.get(url)
+        time.sleep(1)
+        u = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="username"]')))
+        u.send_keys(username)
+        
+        p = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="password"]')))
+        p.send_keys(password)
+        
+        xpath = '//*[@id="login-button"]/input'
+        a = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH,xpath)))
+        a.click()
+        time.sleep(10)
         
     
     
